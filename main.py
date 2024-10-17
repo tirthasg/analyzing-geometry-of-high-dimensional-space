08eb09049d86e9c7a7f27f6fded267b4bea77c31
Tirtha <tirthasg@gmail.com>
2024-10-17 19:36:44 +0530

    feat: get_fraction_outside function implemented for fixed npoints & ndim

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def get_fraction_outside():
 
     rng = np.random.default_rng(seed=42)
     points = rng.random(size=(npoints, ndim))
-    
+
+    squared_distance_from_origin = np.sum(np.square(points), axis=1)
+    mask = squared_distance_from_origin > 1
+    points_outside = points[mask]
+
+    fraction_of_points_outside = points_outside.shape[0] / npoints
+    return fraction_of_points_outside
+
 
 if __name__ == "__main__":
-    get_fraction_outside()
+    print(get_fraction_outside())
