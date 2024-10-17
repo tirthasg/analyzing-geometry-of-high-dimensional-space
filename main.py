7a5145800f1c201bf6b75aa6e700c335da5183ce
Tirtha <tirthasg@gmail.com>
2024-10-17 19:34:05 +0530

    feat: 2d array of numbers in range [0, 1) from an uniform distribution created

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import numpy as np
+
+
 def get_fraction_outside():
     npoints = 100000
     ndim = 2
+
+    rng = np.random.default_rng(seed=42)
+    points = rng.random(size=(npoints, ndim))
+    
 
 if __name__ == "__main__":
     get_fraction_outside()
